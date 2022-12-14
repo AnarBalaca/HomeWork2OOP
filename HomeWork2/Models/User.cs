dff71b87badb7b49f30de5bf6198ea1e9f427bb0
Mranar <anarbalacayev@gmail.com>
2022-12-14 06:41:30 +0400

    Created HomeWork2

---
+++ HomeWork2/Models/User.cs
@@ -0,0 +1,24 @@
+﻿namespace HomeWork2.Models
+{
+    public class User
+    {
+        public string? UserName { get; set; }
+        public string? Email { get; set; }
+
+        public User(string name, string surename)
+        {
+            UserName = $"{name} {surename}".Trim().ToLower(); ;
+            Email = EmailCreator(name, surename);
+
+        }
+
+        
+
+        public string EmailCreator(string name, string sureName)
+        {
+            return this.Email = $"{name}{sureName}@code.edu.az".Trim().ToLower();
+        }
+
+
+    }
+}
---
+++ HomeWork2/Program.cs
@@ -0,0 +1,35 @@
+﻿using HomeWork2.Models;
+
+public class Program
+{
+    public static void Main()
+    {
+        name:
+        Console.WriteLine("Write your Name");
+        string? name = Console.ReadLine();
+        if (name== "")
+        {
+            goto name;
+        }
+        surename:
+        Console.WriteLine("Write your Name");
+        string? surename = Console.ReadLine();
+        if (surename == "")
+        {
+            goto surename;
+        }
+        User user = new User(name, surename);
+        
+        
+        
+
+        
+
+
+        Console.WriteLine($"User:{user.UserName}\nMailAdress:{user.Email} ");
+
+
+
+
+    }
+}
